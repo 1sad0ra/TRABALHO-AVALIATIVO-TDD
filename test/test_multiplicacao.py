26beec0de8e51dd82b8c57269bb75805381a6744
1sad0ra <isadora.cd13@gmail.com>
2026-01-12 23:31:30 -0300

    notas para teste

--- test/test_multiplicacao.py
+++ test/test_multiplicacao.py
@@ -3,3 +3,8 @@ from operacoes.multiplicacao import multiplicar
 def teste_multiplicando_com_sucesso():
     resultado = multiplicar(4, 5)
     assert resultado == 20
+
+#----------Testes para o método multiplicar(a, b)----------#
+#          Deve retornar a multiplicação de dois números positivos;
+#          Deve retornar zero quando um dos valores for zero;
+#          Deve funcionar corretamente com números negativos.
