ad6b8c68e225a1fbc1c6c9d5e6eb57a774368de7
1sad0ra <isadora.cd13@gmail.com>
2026-01-14 14:51:08 -0300

    refatoracao

--- test/test_multiplicacao.py
+++ test/test_multiplicacao.py
@@ -4,6 +4,14 @@ def teste_multiplicando_com_sucesso():
     resultado = multiplicar(4, 5)
     assert resultado == 20
 
+def teste_multiplicando_com_zero():
+    resultado = multiplicar(0, 10)
+    assert resultado == 0
+
+def teste_multiplicando_numeros_negativos():
+    resultado = multiplicar(-3, 6)
+    assert resultado == -18
+
 #----------Testes para o método multiplicar(a, b)----------#
 #          Deve retornar a multiplicação de dois números positivos;
 #          Deve retornar zero quando um dos valores for zero;
