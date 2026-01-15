b297abff87085192c6f2835435941bc90d940588
nicolecs09 <nnaiara585@gmail>
2026-01-14 22:33:06 -0300

    GREENsubtracao aprovado

--- CalculadoraService.py
+++ CalculadoraService.py
@@ -1,7 +1,10 @@
 from operacoes.multiplicacao import multiplicar
+from operacoes.subtracao import subtrair
 
 class Calculadora:
 
     def multiplicacao(self, a, b):
         return multiplicar(a, b)
 
+    def subtracao(self, a, b):
+        return subtrair(a, b)
